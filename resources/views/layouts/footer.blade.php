d7b967c29c020fd3cc6f9ba67053b44e5a90b3aa
Davide <rinaldi.davide94@gmail.com>
2021-01-26 16:44:03 +0100

    init

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,23 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-6 d-flex flex-column align-items-start">
+                <img src="img/logo.png" class='foot_logo' alt="">
+                <address>
+                    Boolean SRL  <br>
+                    Piazzale Giovanni dalle Bande Nere, 9 <br>
+                    20146, Milano <br>
+                    P. IVA: 102345789
+                </address>
+            </div>
+            <div class="col-6 d-flex align-items-end">
+                <ul class="list-unstyled">
+                    <li> <a href="/">Home</a> </li>
+                    <li> <a href="/ilCorso">Il Corso</a> </li>
+                    <li> <a href="/dopoCorso">Dopo il corso</a> </li>
+                    <li> <a href="/camp">Camp Gratuito</a> </li>
+                    <li> <a href="/iscriviti"><button type="button" class="btn btn-outline-success">Cadidati ora</button></a> </li>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,18 @@
+<header>
+    <div class="container nav">
+        <div class="row">
+            <div class="col-4 d-flex align-items-center">
+                <img src="img/logo.png" alt="">
+            </div>
+            <div class="col-8 d-flex align-items-center justify-content-end">
+                <ul class="list-inline list-unstyled">
+                    <li class="list-inline-item"> <a href="/">Home</a> </li>
+                    <li class="list-inline-item"> <a href="/ilCorso">Il Corso</a> </li>
+                    <li class="list-inline-item"> <a href="/dopoCorso">Dopo il corso</a> </li>
+                    <li class="list-inline-item"> <a href="/camp">Camp Gratuito</a> </li>
+                    <li class="list-inline-item"> <a href="/iscriviti"><button type="button" class="btn btn-outline-success">Cadidati ora</button></a> </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
